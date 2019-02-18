b62edd553fde0605550f0adbd34b1cde49bdd7b0
Dennys Lopez Dinza <dlop@hcastechnology.com>
2019-02-18 10:04:29 -0500

    Init Project

---
+++ __tests__/App.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const instance = renderer.create(<App />);
+
+  expect(instance.toJSON()).toMatchSnapshot()
+});
