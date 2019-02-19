0a4810ad1b7546c25256ae08d7f2fcb870af7954
Dennys Lopez Dinza <dlop@hcastechnology.com>
2019-02-19 09:51:01 -0500

    Jest Tests
      - Configured Setup Adapter for Ezyme
      - Add Text to the screen
      - Test the text has expected texts

--- __tests__/App.js
+++ __tests__/App.js
@@ -3,15 +3,33 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import 'react-native';
+import {mount} from 'enzyme';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('App', () => {
+  describe('Form', () => {
+    let app = null
+    beforeEach(() => {
+      app = mount(<App/>)
+    })
 
-it('renders correctly', () => {
-  const instance = renderer.create(<App />);
+    it('should render the world "Length" in testId rectangleLengthText text', () => {
+      const text = app.find('Text').at(0).text()
 
-  expect(instance.toJSON()).toMatchSnapshot()
+      expect(text).toEqual('Length')
+    })
+
+    test.skip('should render the input in testId "rectangleLengthInput"', () => {
+      const input = app.find('TextInput').at(0)
+
+      expect(input).toHaveProperty('placeholder', 'Insert length')
+    })
+
+    it('should render the word "Width" in testID rectangleWitdhText text', () => {
+      const text = app.find("[testID='rectangleWidthText']").at(0).text()
+
+      expect(text).toEqual('Width')
+    })
+  });
 });
